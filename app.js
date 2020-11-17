18d5612a9ed4a109a99d5b203bb9a36918175d44
huudutg <huudutg@gmail.com>
2020-11-18 01:36:47 +0700

    3

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
     })
 );
 app.use(function (req, res, next) {
-    res.header("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+    res.header("Set-Cookie", "HttpOnly;Secure;SameSite=None");
 
     const token = req.cookies.token;
     console.log('req.cookies', req.cookies)
