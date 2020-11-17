b612020bcc418fa384d632c98e128521f74cd7c0
huudutg <huudutg@gmail.com>
2020-11-18 00:52:05 +0700

    fix cors 2

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
     next();
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Origin", "https://funretro-7aa52.web.app/");
+    res.header("Access-Control-Allow-Origin", "https://funretro-7aa52.web.app");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header(
         "Access-Control-Allow-Headers",
